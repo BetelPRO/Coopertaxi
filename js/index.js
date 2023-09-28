b5295dcfe48cff7bd41c3bdec6a83c11a5256ac6
Deivid Procópio <deividprocopio7@gmail.com>
2023-09-28 00:22:55 -0300

    atual

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-document.querySelector('form').addEventListener('submit', handlesubmit);
+let log_nome = getElementById('nome')
+let log_senha = getElementById('senha')
+const bot = getElementById('bot').value
+
+cc
+let conf = {
+    method: 'post',
+    body:JSON.stryngify(usuario),
+}
+
+function Cadastro(){
+    let end = "https://api.sheetmonkey.io/form/kETdvSWYytj6acXznMUMXv"
+    fetch(end)
+    .then(res=>res.json())
+    .then(usuario=>{
+        log_nome.innerHTML=usuario.nome
+        log_senha.innerHTML=usuario.senha
+        alert(usuario.nome)
+    })
+
+    var nome = document.getElementById('nome').value
+    var senha = document.getElementById("senha").value
+    
+    alert(usuario.nome)
+    if(nome == log_nome && senha == log_senha){
+        alert("ok")
+    }else{alert('erro')}
+}
+
---
+++ js/login.js
@@ -0,0 +1,3 @@
+import { Login } from "./index";
+
+Login.login();
