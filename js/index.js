76f8a39ca9f44d850e9ba6cf81b8112a4551a984
Deivid Procópio <deividprocopio7@gmail.com>
2023-09-29 01:29:40 -0300

    sucess

--- js/index.js
+++ js/index.js
@@ -6,12 +6,11 @@ const bot = getElementById('bot').value
 function login(){
     let usuarios = [
         ["deivid","deivid.procopio@ufpe.br","180624"],
-        ["Celia","diogo.tiburcio@ufpe.br","celiaamor"]
-        ['Deivid','deividprocopio7@gmail.com','1806','81986728498']
-        ['Diogo Procópio' ,'diogoprocopio07@gmail.com','diogo123','81986047766']
-        ['Thais','thaisdesouzabm@gmail.com','thais12e','81995117712']
+        ["Celia","diogo.tiburcio@ufpe.br","celiaamor"],
+        ['Deivid','deividprocopio7@gmail.com','1806','81986728498'],
+        ['Diogo Procópio' ,'diogoprocopio07@gmail.com','diogo123','81986047766'],
+        ['Thais','thaisdesouzabm@gmail.com','thais12e','81995117712'],
         ['Manoel', 'manoel-junioir96@hotmail.com', 'man0junior', '81994629566']]
-        
     var nome = document.getElementById('nome').value
     var senha = document.getElementById("senha").value
     let i = 0
@@ -19,11 +18,10 @@ function login(){
         log_nome = usuarios[i][0]
         if(nome == log_nome){
             log_senha = usuarios[i][2]
-            if(senha == log_senha){
+            if(senha == usuarios[i][2]){
                 location.href = "https://betelpro.github.io/Coopertaxi/paginas/pag01.html"
                 alert(log_nome)
-                alert(log_senha)}
-            else{location.href = "https://betelpro.github.io/Coopertaxi"}}}   
+                alert(log_senha)}}}   
 }
 
 function cadastro(){
