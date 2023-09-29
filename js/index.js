30856a1a1e34d9e2d9892e64db5a4a00d4e1a312
Deivid Procópio <deividprocopio7@gmail.com>
2023-09-29 00:14:03 -0300

    atual

--- js/index.js
+++ js/index.js
@@ -1,25 +1,27 @@
 var log_nome = getElementById('nome')
-let log_senha = getElementById('senha')
+var log_senha = getElementById('senha')
 const bot = getElementById('bot').value
-
 
 
 function login(){
     let usuarios = [
         ["deivid","deivid.procopio@ufpe.br","180624"],
-        ["Celia","diogo.tiburcio@ufpe.br","dsqsqds"]]
+        ["Celia","diogo.tiburcio@ufpe.br","dsqsqds"]
+        ['Deivid','deividprocopio7@gmail.com','1806','81986728498']
+        ['Diogo Procópio' ,'diogoprocopio07@gmail.com','diogo123','81986047766']
+        ['Thais','thaisdesouzabm@gmail.com','thais12e','81995117712']]
+        
     var nome = document.getElementById('nome').value
     var senha = document.getElementById("senha").value
-    
     let i = 0
     for(i; i<=2; ++i){
         log_nome = usuarios[i][0]
         if(nome == log_nome){
-            alert(nome)
-            if(nome == log_nome && senha == log_senha){
-                alert("ok")
-            }else{alert('erro')}}
-    }
+            log_senha = usuarios[i][2]
+            if(senha == log_senha){
+                location.href = "https://www.youtube.com/watch?v=SRMEHklQC4M&t=529s"
+                alert(log_nome)
+                alert(log_senha)}}}   
 }
 
 function Cadastro(){
