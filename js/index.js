147d89e098c517fdea3ef47614f27b3adc530711
Deivid Procópio <deividprocopio7@gmail.com>
2023-09-28 21:15:05 -0300

    atual

--- js/index.js
+++ js/index.js
@@ -1,27 +1,32 @@
-let log_nome = getElementById('nome')
+var log_nome = getElementById('nome')
 let log_senha = getElementById('senha')
 const bot = getElementById('bot').value
 
-cc
-let conf = {
-    method: 'post',
-    body:JSON.stryngify(usuario),
-}
 
-function Cadastro(){
-    let end = "https://api.sheetmonkey.io/form/kETdvSWYytj6acXznMUMXv"
-    fetch(end)
-    .then(res=>res.json())
-    .then(usuario=>{
-        log_nome.innerHTML=usuario.nome
-        log_senha.innerHTML=usuario.senha
-        alert(usuario.nome)
-    })
 
+function login(){
+    let usuarios = [
+        ["deivid","deivid.procopio@ufpe.br","180624"],
+        ["Celia","diogo.tiburcio@ufpe.br","dsqsqds"]]
     var nome = document.getElementById('nome').value
     var senha = document.getElementById("senha").value
     
-    alert(usuario.nome)
+    let i = 0
+    for(i; i<=2; ++i){
+        log_nome = usuarios[i][0]
+        if(nome == log_nome){
+            alert(nome)
+            if(nome == log_nome && senha == log_senha){
+                alert("ok")
+            }else{alert('erro')}}
+    }
+}
+
+function Cadastro(){
+    var nome = document.getElementById('nome').value
+    var senha = document.getElementById("senha").value
+    
+    alert(nome)
     if(nome == log_nome && senha == log_senha){
         alert("ok")
     }else{alert('erro')}
--- js/login.js
+++
@@ -1,3 +0,0 @@
-import { Login } from "./index";
-
-Login.login();
