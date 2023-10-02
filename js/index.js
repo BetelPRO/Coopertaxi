ee8fc49a2d4df1655f941c82b27e479c8f249d75
Deivid Procópio <deividprocopio7@gmail.com>
2023-10-02 14:39:52 -0300

    atual

--- js/index.js
+++ js/index.js
@@ -11,17 +11,29 @@ function login(){
         ['Diogo Procópio' ,'diogoprocopio07@gmail.com','diogo123','81986047766'],
         ['Thais','thaisdesouzabm@gmail.com','thais12e','81995117712'],
         ['Manoel', 'manoel-junioir96@hotmail.com', 'man0junior', '81994629566']]
-    var nome = document.getElementById('nome').value
-    var senha = document.getElementById("senha").value
+    let nome = document.getElementById('nome').value
+    let senha = document.getElementById("senha").value
     let i = 0
+    var u_inv = 0
     for(i; i<=5; ++i){
         log_nome = usuarios[i][0]
         if(nome == log_nome){
             log_senha = usuarios[i][2]
-            if(senha == usuarios[i][2]){
-                location.href = "https://betelpro.github.io/Coopertaxi/paginas/pag01.html"}}}   
+            if(senha == log_senha){
+                document.location.replace("https://betelpro.github.io/Coopertaxi/")
+                return
+            }else{
+                alert('Senha incorreta')
+                u_inv = 1}}
+    }
+    if(u_inv == 0){
+        alert("Usuário não encontrado")
+    }
 }
 
+function red(){
+    window.location.replace("../paginas/pag01.html")
+}
 function cadastro(){
     alert('sucess')
 }
