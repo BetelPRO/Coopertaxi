a1fe7ac035a684e2ff4512dc9131034cb96898fe
Deivid Procópio <deividprocopio7@gmail.com>
2023-10-03 12:14:36 -0300

    atual

--- js/index.js
+++ js/index.js
@@ -10,16 +10,17 @@ function login(){
         ['Deivid','deividprocopio7@gmail.com','1806','81986728498'],
         ['Diogo Procópio' ,'diogoprocopio07@gmail.com','diogo123','81986047766'],
         ['Thais','thaisdesouzabm@gmail.com','thais12e','81995117712'],
-        ['Manoel', 'manoel-junioir96@hotmail.com', 'man0junior', '81994629566']]
-    let nome = document.getElementById('nome').value
-    let senha = document.getElementById("senha").value
+        ['Manoel', 'manoel-junioir96@hotmail.com', 'man0junior', '81994629566'],
+        ['Daniel', 'danieelcordeiro325@gmail.com', '19042002', '81985944217']]
+    let nome = log_nome.value
+    let senha = log_senha.value
     let i = 0
     var u_inv = 0
     for(i; i<=5; ++i){
         userNome = usuarios[i][0]
         userSenha = usuarios[i][2]
-        if(nome == userNome){
-            if(senha == userSenha){
+        if(nome === userNome){
+            if(senha === userSenha){
                 window.open("https://betelpro.github.io/Coopertaxi/paginas/pag01.html")
                 window.close("https://betelpro.github.io/Coopertaxi")
                 return;
@@ -27,8 +28,9 @@ function login(){
                 alert('Senha incorreta')
                 u_inv = 1}}
     }
-    if(u_inv == 0){
+    if(u_inv === 0){
         alert("Usuário não encontrado")
+        return;
     }
 }
 
